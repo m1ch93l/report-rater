280979a648f6a4ea4aad9ebf6ef25c1c80eea1c1
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-06-06 15:55:10 +0800

    replace not rated

--- admin/participant-list-update.php
+++ admin/participant-list-update.php
@@ -7,15 +7,19 @@ $stmt = $conn->prepare($sql);
 $stmt->execute(); 
 $ratings = $stmt->fetchAll();
 
-$sql2 = "SELECT id, fullname FROM participant WHERE id NOT IN (SELECT DISTINCT rater_id FROM rate)";
+$sql2 = "SELECT id, fullname, status FROM participant WHERE id NOT IN (SELECT DISTINCT rater_id FROM rate)";
 $stmt2 = $conn->prepare($sql2);
 $stmt2->execute();
 $notRated = $stmt2->fetchAll();
 
 if (count($notRated) > 0) {
-    echo "Who did not rated<br>";
-    foreach ($notRated as $participant) {
-        echo $participant['fullname'] . ', ';
+    echo "Who hasn't been rated yet:";
+
+    foreach ($notRated as $participant) { 
+        if ($participant['status'] == 1) {
+            echo '<p class="text-capitalize text-success">Online - ' . $participant['fullname'] . '</p>';
+        } else {
+            echo '<p class="text-capitalize text-danger">Offline - ' . $participant['fullname'] . '</p>';
+        }
     }
-    echo "<br>";
 }
