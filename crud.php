9f38fc5b271c24dbc8903dee8b0d3139d551336f
Michael B. Bañaria <mbbmichaelbolobanaria25@gmail.com>
2025-06-07 16:29:10 +0800

    fix admin login

--- crud.php
+++ crud.php
@@ -1,15 +1,15 @@
 <?php
-
 session_start();
-require_once 'includes/database.php';
+//require_once 'includes/database.php';
+require_once 'model/user.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // for registration of participant
+    //for registration of participant
     if (isset($_POST['register'])) {
-        $participant = $_POST['participant'];
-        $password    = $_POST['password'];
-        $name        = $_POST['name'];
-        $group_belong = $_POST['group_belong'];
+        $participant        = $_POST['participant'];
+        $password           = $_POST['password'];
+        $name               = $_POST['name'];
+        $group_belong       = $_POST['group_belong'];
         $year_level_section = $_POST['year_level_section'];
 
         $sql  = "INSERT INTO participant (participant_id, password, fullname, group_belong, year_level_section) VALUES (:participant, :password, :name, :group_belong, :year_level_section)";
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $stmt->bindParam(':name', $name);
         $stmt->bindParam(':group_belong', $group_belong);
         $stmt->bindParam(':year_level_section', $year_level_section);
-        
+
         if ($stmt->execute()) {
             $_SESSION['registered'] = true;
             header('location: index');
@@ -33,6 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // for login of participant
     if (isset($_POST['login'])) {
+
         $participant = $_POST['participant'];
         $password    = $_POST['password'];
 
@@ -46,14 +47,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         } else {
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
             if ($row['password'] == $password) {
-                $_SESSION['participant'] = $row['id'];
-                $_SESSION['fullname']    = $row['fullname'];
-                $_SESSION['hide'] = false;
-                $_SESSION['group_belong'] = $row['group_belong'];
+                $_SESSION['participant']   = $row['id'];
+                $_SESSION['fullname']      = $row['fullname'];
+                $_SESSION['hide']          = false;
+                $_SESSION['group_belong']  = $row['group_belong'];
                 $_SESSION['online_status'] = $row['online_status'];
 
                 // Update status to online
-                $updateSql = "UPDATE participant SET online_status = 1 WHERE id = :id";
+                $updateSql  = "UPDATE participant SET online_status = 1 WHERE id = :id";
                 $updateStmt = $conn->prepare($updateSql);
                 $updateStmt->bindParam(':id', $row['id']);
                 $updateStmt->execute();
@@ -81,8 +82,26 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             }
         }
 
+        // using admin function class of user from model
+        $user  = new User();
+        $admin = $user->findAdminUser($_POST['participant']);
+        if ($admin) {
+            if (password_verify($password, $admin['password'])) {
+                $_SESSION['admin'] = $admin['id'];
+                header('location: admin/home');
+                exit();
+            } else {
+                $_SESSION['error'] = 'Incorrect password';
+                exit();
+            }
+        } else {
+            $_SESSION['error'] = 'Cannot find account with the username';
+            exit();
+        }
+
     } else {
         $_SESSION['error'] = 'Input voter credentials first';
+        exit();
     }
 
     header('location: home');
